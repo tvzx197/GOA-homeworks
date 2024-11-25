878c7d73890cafa911bfb2a55cadba79fe0b5b97
tvzx197 <tevzadzetato8@gmail.com>
2024-11-26 03:15:42 +0400

    commit

---
+++ day15/homework/hw.py
@@ -0,0 +1,8 @@
+int=int(input("enter integer"))
+if int > 0 and int < 1000:
+    print("positive integer")
+elif  int >-1000 and int <0:
+    print("negative integer")
+else :
+    print("zero integer")
+    
---
+++ day15/homework/hw2.py
@@ -0,0 +1,11 @@
+mark=int(input("enter your mark"))
+if mark >90 and mark <100:
+    print = ("A")
+elif mark >80 and mark <90:
+    print("B")
+elif mark >70 and mark <80:
+    print("C")
+elif mark >60 and mark <70:
+    print("D")
+
+else: print("f")
