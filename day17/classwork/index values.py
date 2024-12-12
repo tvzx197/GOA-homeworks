b949f1cbf8db38a5ebf4fec033a1d99b5cbb9d39
tvzx197 <tevzadzetato8@gmail.com>
2024-12-13 00:11:01 +0400

    commit

---
+++ day17/classwork/index values.py
@@ -0,0 +1,14 @@
+
+values = ["bmw", "Mercedes", "Subaru", "Mazerati", "Ford", "mustang", "Skoda", "Volkswagen", "Ferrari",  "Nissan"]
+print(values[0])
+print(values[1])
+print(values[2])
+values[3]="GL"
+values[4]="Ford mustang"
+values[3]="Octavia"
+print(values)
+
+values.append("mazda")
+values.append("M5")
+print(values)
+
---
+++ day17/homework/hw.py
@@ -0,0 +1,10 @@
+for i in range(5):
+    user_number = input("Enter number: ")
+
+
+if user_number > 10:
+    print("სიაში არის 10 ზე მაღალი რიცხვი")
+else:
+    print("სიაში არ არის 10 ზე მაღალი რიცხვი")
+
+print(sum(user_number))
