e566d1878bd8b7572a4c5277ded2a1d06a3ce371
tvzx197 <tevzadzetato8@gmail.com>
2024-12-14 23:14:45 +0400

    commit

---
+++ day19/classwork/classwork.py
@@ -0,0 +1,16 @@
+#answer is 4
+#for example 
+#string=["h", "o", "m", "e"]
+#print(string[3])
+#მხოლოდ ბოლო ასო გამოვიტანეთ
+#for instance
+#string=['n', "a", "m", "e", "s"]
+#print(string[1:-1])
+#-1 არის უარყოფითი და შესაბამისად "s "არის ბოლო ასო და ერთის გამოკლებით იწყება, ანალოგიურადაა დასასწყისშიც "n"-ს გარდა ყველა გამოდის
+#ანუ მხოლოდ გამოვა ame
+#სიტყვის შემობრუნდებული ვერსიაა
+#text=["H", "e", "l", "l", "o" " ", "w", "o", "r", "l", "d"]
+#print(text[::-1])
+text=hello world
+reverse_text
+print (text)
