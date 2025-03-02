6e780e95a635a9f2936a100fee731c8332f39c71
tvzx197 <tevzadzetato8@gmail.com>
2025-03-02 23:46:52 +0400

    commit

---
+++ day42/homework/hw1.py
@@ -0,0 +1,3 @@
+def set_alarm(employed, vacation):
+    return employed and not vacation
+    # Your code here
---
+++ day42/homework/hw2.py
@@ -0,0 +1,10 @@
+def update_light(current):
+    if current == "green":
+        return "yellow"
+    elif current == "yellow":
+        return "red"
+    elif current == "red":
+        return "green"
+    else:
+        return "Invalid input"
+    # Your code here.
