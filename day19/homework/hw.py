f853dd695f41238149dfd97d81509e9cfdcab5a6
tvzx197 <tevzadzetato8@gmail.com>
2025-06-04 22:28:27 +0400

    commit

--- day19/homework/hw.py
+++ day19/homework/hw.py
@@ -1,11 +1,6 @@
-# Initialize an empty list to store the numbers
 numbers = []
-# Use a loop to collect 10 numbers from the user
 for i in range(10):
-    # Ask the user to input a number
-    num = int(input("Please enter a number: "))  # You can adjust the type based on your needs
-    # Append the number to the list
+    num = int(input("Please enter a number: "))  
     numbers.append(num)
-# Print the collected list of numbers
 print("The collected numbers are:", numbers)
 
